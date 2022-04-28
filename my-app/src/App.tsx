641bb5c0ca4900fa7e7ad031ec04456c7754214c
charlottefurn <64796525+charlottefurn@users.noreply.github.com>
2022-04-28 17:39:49 +0100

    More style tweaks for Bulma

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import { ReactElement, useState } from "react";
 import { Search } from "./Search.tsx";
 import { Details } from "./Details.tsx";
+import { Contact } from "./Contact.tsx";
 
 const App = (): ReactElement => {
   return (
-      <div className="wrapper is-flex is-flex-direction-column is-align-items-center">
+      <div className="is-flex is-flex-direction-column is-align-items-center">
         <h2 className="heading">devfinder</h2>
         <Details />
+        <Contact />
       </div>
 
   );
--- my-app/src/Contact.tsx
+++ my-app/src/Contact.tsx
@@ -1,11 +1,11 @@
 import { ReactElement } from "react";
 
-const Contact = (): ReactElement => {
+export const Contact = (): ReactElement => {
     return (
-        <div className="contact">
+        <div className="contact is-flex is-flex-direction-column">
         <p>Created by Charlotte Furness</p>
-        <div className="icons">
-          <a target="_blank" href="https://github.com/charlottefurn"><i class="fab fa-github"></i></a>
+        <div className="icons is-flex is-flex-direction-row is-justify-content-space-evenly">
+          <a target="_blank" href="https://github.com/charlottefurn"><i className="fab fa-github"></i></a>
           <a target="_blank" href="https://www.linkedin.com/in/charlotte-furness-859027211/"><i class="fab fa-linkedin"></i></a>
         </div>
       </div>

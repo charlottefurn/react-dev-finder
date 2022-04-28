88fe8c6353800d3b51b3bb94eb21168661b5f220
charlottefurn <64796525+charlottefurn@users.noreply.github.com>
2022-04-28 16:47:05 +0100

    removes unused code

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,24 +4,11 @@ import { Search } from "./Search.tsx";
 import { Details } from "./Details.tsx";
 
 const App = (): ReactElement => {
-
-
-  // useEffect(() => {
-  // fetch("https://api.github.com/users/")  //needs to get name from search and pass into here???
-
-  // .then((result) => result.json())
-  // .then((data) => {
-  //     console.log(data);
-  //     setUser(data)
-  // });
-  // }, [])
-
   return (
     <>
       <h2 className="heading">devfinder</h2>
 
       <div className="wrapper">
-        {/* <Search user={user} /> */}
         <Details />
       </div>
     </>
